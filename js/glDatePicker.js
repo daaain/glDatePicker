c732bb7f66026428fb7938e741c5327d3ab1fead
Daniel Demmel <daniel.demmel@gmail.com>
2012-02-03 13:49:12 +0000

    Removing "float:left;" from inner div and with that getting rid of dynamically set "top" property in JS – this makes using the date picker in "showAlways" mode much nicer by having the outer div size wrap itself on the inner one, making it possible to align the date picker centred horizontally, etc. Only caveat is that I haven't tested this in IE…

--- js/glDatePicker.js
+++ js/glDatePicker.js
@@ -296,8 +296,7 @@
 					{
 						"position":settings.position,
 						"z-index":settings.zIndex,
-						"left":(target.offset().left),
-						"top":target.offset().top+target.outerHeight(true)
+						"left":(target.offset().left)
 					})
 				);
 			}
